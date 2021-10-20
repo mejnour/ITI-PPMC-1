52633c9883dcb049866c4f2a9daa4c402b913211
mejnour <apina.dev@gmail.com>
2021-10-20 00:06:05 -0300

    feat: dividing to conquer

--- encoder.py
+++ encoder.py
@@ -1,48 +1,44 @@
-import struct
 import sys
-import time
+from sys import argv
+from struct import *
 
-def encoder(file):
-  openedFile = open(file,"rb")
-  decodedFile = open('saida_' + file + '.lzw', 'wb')
 
-  data = openedFile.read(1024*1024)
+def encoder(file):    
+    openedFile = open(file,'rb')                 
+    data = openedFile.read(1024*1024)                      
 
-  dictionary_size = 256                   
-  dictionary = {str(i):i for i in range(dictionary_size)}
+    dictionary_size = 256                   
+    dictionary = {str(i): i for i in range(dictionary_size)}    
+    string = ""
+    compressed_data = []
 
-  #print(dictionary)
+    while data:
+        for symbol in data:                     
+            string_plus_symbol = string + str(symbol)
+            if string_plus_symbol in dictionary: 
+                string = string_plus_symbol
+            else:
+                compressed_data.append(dictionary[string])
+                if(len(dictionary) <= 65535):
+                    dictionary[string_plus_symbol] = dictionary_size
+                    dictionary_size += 1
+                string = str(symbol)
+        data = openedFile.read(1024*1024)
 
-  print(data)
-#   sys.exit()
-  
-  nss='' #Next.Symbol.Sequence
-  kss='' #Known.Symbol.Sequence
-  while data:
-      for symbol in data:
-          print('symb ', symbol)
+    if string in dictionary:
+        compressed_data.append(dictionary[string])
 
-          nss=kss+str(dictionary[str(symbol)])
-        #   sys.exit()
-
-          nss=kss+str(dictionary[symbol])
-          sys.exit()
-          
-          print('symb ', symbol, 'nss ', nss, 'kss ', kss, sep='|')
-          if nss in dictionary:
-              kss=nss
-          else:
-              #ofile.write(bytearray(bin(dictionary[kss]),'utf-8'))
-              decodedFile.write(struct.pack('i', dictionary[kss]))
-              dictionary[nss] = dictionary_size
-              dictionary_size += 1
-              kss = str(symbol)
-      data = openedFile.read(1024*1024)
-  decodedFile.close()
-  openedFile.close()
+    out = file.split(".")[0]
+    output_file = open(out + ".lzw", "wb")
+    print(out)
+    for data in compressed_data:
+        output_file.write(pack('>H',int(data)))
+        
+    output_file.close()
+    openedFile.close()
 
 def run(file):
   encoder(file)
 
 if __name__ == '__main__':
-    run('teste1.txt')
+    run('ArnaldoPina.txt')
