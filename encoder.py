4cffa232ded363932fb3a3757e8364d4d17ff7a3
mejnour <apina.dev@gmail.com>
2021-10-22 22:17:44 -0300

    feat: small improv

--- encoder.py
+++ encoder.py
@@ -3,19 +3,38 @@ from sys import argv
 import struct
 from struct import *
 import helper
+import os
+import enum
 
+
+class SIZE_UNIT(enum.Enum):
+   BYTES = 1
+   KB = 2
+   MB = 3
+   GB = 4
+
+def convert_unit(size_in_bytes, unit):
+  """ Convert the size from bytes to other units like KB, MB or GB"""
+  if unit == SIZE_UNIT.KB:
+      return size_in_bytes/1024
+  elif unit == SIZE_UNIT.MB:
+      return size_in_bytes/(1024*1024)
+  elif unit == SIZE_UNIT.GB:
+      return size_in_bytes/(1024*1024*1024)
+  else:
+      return size_in_bytes
 
 def encoder(file):
   print('.', end='', flush=True)
   openedFile = open(file,'rb')                 
   data = openedFile.read()                      
-  k=16
+  k=12
   dictionarySize = 256
 
   dictionary = {}
   for i in range(dictionarySize):
       dictionary[str(i)] = i
-      
+
   currentStr = ""
   lzwCompressed = []
 
@@ -47,11 +66,19 @@ def encoder(file):
 
   print('.', end=' ', flush=True)
 
+def get_file_size(file_name, size_type = SIZE_UNIT.MB ):
+   """ Get file in size in given unit like KB, MB or GB"""
+   size = os.path.getsize(file_name)
+   return convert_unit(size, size_type)
+
+
 def run(file):
   bEncoding, gbDone, bFile  = helper.formatColors('enc', file)
   print(f'LZW {bEncoding} for file {bFile} has started ', end='', flush=True)
   encoder(file)
   print(f'{gbDone}')
+
+  print(get_file_size(file))
 
 if __name__ == '__main__':
     run('ArnaldoPina.txt')
